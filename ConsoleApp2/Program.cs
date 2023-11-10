5d56a9351ebaa3a059f10be86e3caa8f9d1c9f55
Ilya <73891063+Pluumbus@users.noreply.github.com>
2023-11-10 17:58:35 +0600

    Add files via upload

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -22,11 +22,16 @@ for (int i = year; days > 364; days -= 365 )
 }
 
 int[] monthDays = { 31, year % 4 == 0 ? 29 : 28 , 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-for (int i = month; days >= monthDays[month - 1]; days -= monthDays[month - 1] )
-{
-    if (year % 4 == 0 && month == 2) { days -= 2; break; } else { month++; }
-}
 
+for (int i = month; days >= monthDays[i - 1]; days -= monthDays[month - 1] )
+{
+    month++;
+
+}
+if (month == 2 && days >= 28)
+{
+    days -= 2;
+}
 
 int day = Convert.ToInt32(days);
 int hour = Convert.ToInt32(hours % 24);
@@ -44,9 +49,7 @@ else
 }
 
 string millisec = milliseconds.Length == 1 ? "00" + milliseconds : milliseconds.Length == 2 ? "0" + milliseconds : milliseconds;
-
-string yearHigh = (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) ? "Високосный" : "Не високосный" ;
 day += dayS;
-
+string yearHigh = (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) ? "Високосный" : "Не високосный" ;
 string output = String.Format("{0:d2}.{1:d2}.{2} {3:d2}:{4:d2}:{5:d2}.{6} {7}",day,month,year,hour,minute,second,millisec,yearHigh);
 Console.WriteLine(output);
